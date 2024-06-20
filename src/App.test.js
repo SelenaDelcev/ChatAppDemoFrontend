4d76e6489d411c6b039b9dacedc38dd21039f45e
SelenaDelcev <selenadelcev411@gmail.com>
2024-06-20 19:14:32 +0200

    update test files

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders assistant message', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const assistantMessage = screen.getByText(/You are a helpful assistant/i);
+  expect(assistantMessage).toBeInTheDocument();
 });
